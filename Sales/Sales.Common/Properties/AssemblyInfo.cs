c83056f7165e57254d2968348179e0cfb8e99034
Misael Rodriguez <rodriguez.misael@gmail.com>
2019-07-14 18:26:55 -0400

    Crear el modelo Product

--- Sales/Sales.Common/Properties/AssemblyInfo.cs
+++ Sales/Sales.Common/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sales.Commond")]
+[assembly: AssemblyTitle("Sales.Common")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Sales.Commond")]
+[assembly: AssemblyProduct("Sales.Common")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e96af7f3-b64c-401e-a84e-e2b1a5b04a90")]
+[assembly: Guid("3091afda-fe37-4a29-932f-12da1c010c88")]
 
 // Version information for an assembly consists of the following four values:
 //
