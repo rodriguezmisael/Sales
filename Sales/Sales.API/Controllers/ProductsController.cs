3118dfb79e300bd4da5cf060b8e0fc5d60a5e748
Misael Rodriguez <rodriguez.misael@gmail.com>
2019-07-17 08:09:48 -0400

    Arreglar controller API

--- Sales/Sales.API/Controllers/ProductsController.cs
+++ Sales/Sales.API/Controllers/ProductsController.cs
@@ -1,14 +1,19 @@
-﻿namespace Sales.API.Controllers
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using Sales.Common.Models;
+using Sales.Domain.Models;
+
+namespace Sales.API.Controllers
 {
-    using Sales.Common.Models;
-    using Sales.Domain.Models;
-    using System.Data.Entity;
-    using System.Data.Entity.Infrastructure;
-    using System.Linq;
-    using System.Net;
-    using System.Threading.Tasks;
-    using System.Web.Http;
-    using System.Web.Http.Description;
     public class ProductsController : ApiController
     {
         private DataContext db = new DataContext();
--- Sales/Sales.Domain/Models/DataContext.cs
+++ Sales/Sales.Domain/Models/DataContext.cs
@@ -1,14 +1,15 @@
 ﻿namespace Sales.Domain.Models
 {
+    using Sales.Common.Models;
     using System.Data.Entity;
-
-    
+    using System.Linq;
 
     public class DataContext : DbContext
     {
+        public readonly IQueryable<Product> Product;
+
         public DataContext() : base("DefaultConnection")
         {
-
         }
 
         public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
